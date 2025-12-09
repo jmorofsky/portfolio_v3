5ec666a7313e59ce9a5646bb52f08d59bc2475a9
jmorofsky <jmorofsky@gmail.com>
2025-12-08 19:14:32 -0500

    create project structure and build tools

---
+++ build.mjs
@@ -0,0 +1,49 @@
+import tailwindcss from '@tailwindcss/postcss';
+import { readFile } from 'node:fs/promises';
+import postcss from 'postcss';
+import * as esbuild from 'esbuild';
+
+
+const postcssPlugin = ({ plugins = [] } = {}) => {
+    return {
+        name: 'postcss',
+        setup(build) {
+            build.onLoad({ filter: /\.css$/ }, async (args) => {
+                const raw = await readFile(args.path, 'utf8');
+                const source = await postcss(plugins).process(raw, {
+                    from: args.path,
+                });
+                return {
+                    contents: source.css,
+                    loader: 'css'
+                };
+            });
+        },
+    };
+};
+
+const buildOptions = {
+    entryPoints: ['src/index.jsx'],
+    bundle: true,
+    outfile: 'dist/bundle.js',
+    inject: ['./react-shim.js'],
+    plugins: [
+        postcssPlugin({ plugins: [tailwindcss] }),
+    ],
+    loader: { '.jsx': 'jsx' },
+    minify: false
+}
+
+async function runBuildAndWatch() {
+    try {
+        const ctx = await esbuild.context(buildOptions);
+        await ctx.watch();
+
+        console.log('[esbuild] watching for changes...');
+    } catch (err) {
+        console.error('[esbuild] build failed:', err);
+        process.exit(1);
+    }
+}
+
+runBuildAndWatch();
---
+++ prod-build.mjs
@@ -0,0 +1,36 @@
+import { build } from 'esbuild';
+import postcss from 'postcss';
+import { readFile } from 'node:fs/promises';
+import tailwindcss from '@tailwindcss/postcss';
+
+
+const postcssPlugin = ({ plugins = [] } = {}) => {
+    return {
+        name: 'postcss',
+        setup(build) {
+            build.onLoad({ filter: /\.css$/ }, async (args) => {
+                const raw = await readFile(args.path, 'utf8');
+                const source = await postcss(plugins).process(raw, {
+                    from: args.path,
+                });
+                return {
+                    contents: source.css,
+                    loader: 'css'
+                };
+            });
+        },
+    };
+};
+
+await build({
+    entryPoints: ['src/index.jsx'],
+    bundle: true,
+    outfile: 'dist/bundle.js',
+    inject: ['./react-shim.js'],
+    plugins: [
+        postcssPlugin({ plugins: [tailwindcss] }),
+    ],
+    loader: { '.jsx': 'jsx' },
+    minify: true,
+    sourcemap: false
+}).catch(() => process.exit(1));
