78d7b22b541e2c340e4e786efeef81ac3baf9410
jmorofsky <jmorofsky@gmail.com>
2025-12-15 21:49:00 -0500

    add contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,3 +1,37 @@
 export default function Contact() {
-    return <div className='opacity-0 animate-fadeIn'>Contact</div>;
+    return (
+        <div className='flex flex-col gap-1 absolute bottom-px right-px m-14 font-bold items-end'>
+            <a
+                href='https://www.linkedin.com/in/jason-morofsky/'
+                className='hover:text-amber-200 w-fit'
+                style={{ transition: 'all 0.33s' }}
+            >LinkedIn ↗
+            </a>
+
+            <a
+                href='https://github.com/jmorofsky/'
+                className='hover:text-amber-200 w-fit'
+                style={{ transition: 'all 0.33s' }}
+            >
+                GitHub ↗
+            </a>
+
+            <a
+                href='mailto:contact@jasonmorofsky.com'
+                className='hover:text-amber-200 w-fit'
+                style={{ transition: 'all 0.33s' }}
+                title='Email Me'
+            >
+                contact@jasonmorofsky.com ↗
+            </a>
+
+            <a
+                href="https://storage.googleapis.com/jasonmorofsky/JasonMorofsky'sResume.pdf"
+                className='hover:text-amber-200 w-fit'
+                style={{ transition: 'all 0.33s' }}
+            >
+                Resume ↓
+            </a>
+        </div>
+    );
 };
